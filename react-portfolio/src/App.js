100f139f660248fef989ed62eaa87bcae72decd4
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-20 17:38:57 -0600

    added navbar to page, and styled it, deciding on which colors to use

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <Card />
       <Footer />
