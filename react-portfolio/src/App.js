ee876dd0980e17a6b3be80e4b427c4393e0fadc7
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-20 16:29:37 -0600

    added footer to page, and edited header, added image to card, changed card font color

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Card from "./components/Card/Card";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Card />
+      <Footer />
     </>
   );
 }
--- react-portfolio/src/components/Card/Card.js
+++ react-portfolio/src/components/Card/Card.js
@@ -3,28 +3,25 @@ import "./Card.css"
 
 function Card(props) {
     return (
-        <div className="card text-center">
-            <div className="card-header">
-                Portfolio
-  </div>
+        <div className="card text-center justify-content-center" >
             <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-            <div className="card-body" className="float-right" >
+                <img src="..." alt="app-pic"></img>
                 <h5 className="card-title">Special title treatment</h5>
                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
             <div className="card-body">
+                <img src="..." alt="app-pic" ></img>
                 <h5 className="card-title">Special title treatment</h5>
                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
-            <div className="card-footer text-muted">
-                Apps made by Nicholas C. Maas
-  </div>
+            <div className="card-body">
+                <img src="..." alt="app=pic"></img>
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
 
     )
