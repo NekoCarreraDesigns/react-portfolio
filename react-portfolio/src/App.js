934ca8f6830a23ee6ceadce695b71cc41c784756
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-22 00:00:29 -0600

    added my picture to the app, added some description, and styled the body section

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import Body from "./components/Body/Body";
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
--- react-portfolio/src/components/Body/Body.js
+++ react-portfolio/src/components/Body/Body.js
@@ -3,8 +3,13 @@ import "./Body.css";
 
 function Body() {
     return (
+        <div className="sea-and-sand" >
+            <p className="intro" >Hello My name is Nicholas C. Maas, I am a graduate of the University of Denver Coding Boot Camp. I am a Full Stack Web Developer, I love to design, and enjoy the front-end because of my art background. However I do enjoy the Back end as well</p>
+            <img className="sunset-surf" alt="sunset-surfer" src="https://images.unsplash.com/photo-1475615787683-f1a161b002b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            <img className="aerial" alt="aerial-san-diego" src="https://images.unsplash.com/photo-1544632795-2e45e65a8ce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            <img className="nicholas" alt="handsome-gentleman" src="/Nicholas1.jpeg"></img>
+        </div>
 
-        <img className="sunset-surf" src="https://images.unsplash.com/photo-1475615787683-f1a161b002b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
     )
 }
 
