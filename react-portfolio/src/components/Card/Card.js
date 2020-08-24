05d195cea361f5e194c69f7f1611f47198c6c728
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-24 16:11:11 -0600

    did some cleanup of the card, and body files

--- react-portfolio/src/components/Card/Card.js
+++ react-portfolio/src/components/Card/Card.js
@@ -11,9 +11,8 @@ function Card() {
                         <h3 id="signage" className="mt-0 mb-1">Weather Dashboard</h3>
                         <p id="menu"> Weather dashboard lets you view the weather for any city you want to search, want to know how the
                         weather is at moms house, or in your home town, maybe you plan on traveling and don't want to forget to pack
-                        a
-                        jacket or forget your flip flops, the app uses the Open Weather API, and jQuery to function
-    </p>
+                        a jacket or forget your flip flops, the app uses the Open Weather API, and jQuery to function
+                        </p>
                         <button style={{ backgroundColor: 'slateblue' }} type="button" className="btn btn-primary btn-lg"><a className="button" href="https://nekocarreradesigns.github.io/weather-dashboard/">Whew it's a scorcher!</a></button>
                         <button style={{ backgroundColor: 'slateblue' }} type="button" className="btn btn-primary btn-lg"><a className="button" href="https://github.com/NekoCarreraDesigns/weather-dashboard">Check Out The Code!</a></button>
                     </div>
@@ -81,28 +80,9 @@ function Card() {
                         <button style={{ backgroundColor: 'slateblue' }} type="button" className="btn btn-primary btn-lg"><a className="button" href="https://github.com/NekoCarreraDesigns/note-taker">Check Out The Code!</a></button>
                     </div>
                 </li>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             </ul>
         </div>
 
     )
 }
-
 export default Card;
