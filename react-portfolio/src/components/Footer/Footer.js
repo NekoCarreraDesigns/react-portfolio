b3c3d06272b5691ad790f75dd713017efd18aaf0
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-24 23:30:05 -0600

    added fluid container to the footer

--- react-portfolio/src/components/Footer/Footer.js
+++ react-portfolio/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 function Footer(props) {
     return (
-        <footer className="text-center ">
+        <footer className="text-center container-fluid">
             <div className="card-footer">
                 <small className="text-red mx-auto">
                     &copy; 2020 • All Rights Reserved
