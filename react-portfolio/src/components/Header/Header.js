678b912a1bb21059e5123670de763ad0476fea2b
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-27 14:23:36 -0600

    added fluid tag to image and container

--- react-portfolio/src/components/Header/Header.js
+++ react-portfolio/src/components/Header/Header.js
@@ -4,8 +4,8 @@ import "./Header.css";
 function Header(props) {
     return (
         <div>
-            <h1 className="page-head">
-                <img className="page-head" src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+            <h1 className="page-head container-fluid">
+                <img className="page-head img-fluid" alt="computer-code" src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
                 Welcome to Neko Carrera Designs
             </h1>
 
