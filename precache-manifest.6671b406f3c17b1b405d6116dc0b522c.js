26cabcb638f4600c783902c4ccd0d6060e3d673e
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-27 14:44:22 -0600

    Updates

---
+++ precache-manifest.6671b406f3c17b1b405d6116dc0b522c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c011d577e541324fc039d3941d658732",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1663fc1703f31f3a68bf",
+    "url": "/react-portfolio/static/css/main.7999e892.chunk.css"
+  },
+  {
+    "revision": "6623143cadbf5393a5f2",
+    "url": "/react-portfolio/static/js/2.357adb26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.357adb26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1663fc1703f31f3a68bf",
+    "url": "/react-portfolio/static/js/main.c15c620a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
