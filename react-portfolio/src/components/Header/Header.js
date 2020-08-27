9592eb6d09921fc19898cf901893edf1e7b57046
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-27 13:41:26 -0600

    added image to header, and added some padding to the contact, and header pages

--- react-portfolio/src/components/Header/Header.js
+++ react-portfolio/src/components/Header/Header.js
@@ -5,7 +5,10 @@ function Header(props) {
     return (
         <div>
             <h1 className="page-head">
-                Welcome to Neko Carrera Designs </h1>
+                <img className="page-head" src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+                Welcome to Neko Carrera Designs
+            </h1>
+
         </div>
     )
 }
